dd72a207ce9981991e3b175a82f23dd20a6f9511
fabio.collini <fabio.collini@19801066-89cc-11de-a519-b78fc8232508>
2009-08-23 18:45:24 +0000

    Edit label button in shortcut folder title
    git-svn-id: http://appsorganizer.googlecode.com/svn/trunk/AppsOrganizer@30 19801066-89cc-11de-a519-b78fc8232508

--- src/com/google/code/appsorganizer/shortcut/LabelShortcut.java
+++ src/com/google/code/appsorganizer/shortcut/LabelShortcut.java
@@ -23,17 +23,28 @@ import java.util.Collection;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.code.appsorganizer.ApplicationInfoManager;
+import com.google.code.appsorganizer.AppsReloader;
+import com.google.code.appsorganizer.ChooseAppsDialogCreator;
 import com.google.code.appsorganizer.R;
 import com.google.code.appsorganizer.db.DatabaseHelper;
 import com.google.code.appsorganizer.db.DbChangeListener;
+import com.google.code.appsorganizer.dialogs.GenericDialogManager;
 import com.google.code.appsorganizer.model.AppLabel;
 import com.google.code.appsorganizer.model.Application;
 import com.google.code.appsorganizer.model.GridObject;
@@ -54,6 +65,14 @@ public class LabelShortcut extends Activity implements DbChangeListener {
 
 	private static Label ALL_LABELS;
 
+	private TextView titleView;
+
+	private ImageView titleButton;
+
+	private ChooseAppsDialogCreator chooseAppsDialogCreator;
+
+	private GenericDialogManager genericDialogManager;
+
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
@@ -62,10 +81,20 @@ public class LabelShortcut extends Activity implements DbChangeListener {
 		applicationInfoManager.reloadAppsMap();
 		dbHelper = DatabaseHelper.initOrSingleton(this);
 
+		genericDialogManager = new GenericDialogManager(this);
+		chooseAppsDialogCreator = new ChooseAppsDialogCreator(dbHelper, applicationInfoManager);
+		genericDialogManager.addDialog(chooseAppsDialogCreator);
+
 		if (ALL_LABELS == null) {
 			ALL_LABELS = new Label(LabelShortcut.ALL_LABELS_ID, getString(R.string.all_labels), R.drawable.icon);
 		}
 		final Intent intent = getIntent();
+
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		getOrCreateGrid();
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_shortcut_title);
+		titleView = (TextView) findViewById(R.id.title_text);
+		titleButton = (ImageView) findViewById(R.id.editButton);
 
 		long labelId = intent.getLongExtra(LABEL_ID, ALL_LABELS_ID);
 		if (labelId == ALL_LABELS_ID) {
@@ -75,7 +104,7 @@ public class LabelShortcut extends Activity implements DbChangeListener {
 			allLabelsSelected = false;
 			label = dbHelper.labelDao.queryById(labelId);
 		}
-		getOrCreateGrid();
+
 		reloadGrid();
 	}
 
@@ -89,14 +118,23 @@ public class LabelShortcut extends Activity implements DbChangeListener {
 		if (label != null) {
 			@SuppressWarnings("unchecked")
 			final AppGridAdapter<GridObject> gridAdapter = (AppGridAdapter<GridObject>) grid.getAdapter();
-			setTitle(label.getName());
+			titleView.setText(label.getName());
 			if (label.getId() == ALL_LABELS_ID) {
 				List<Label> labels = dbHelper.labelDao.getLabels();
 				gridAdapter.setObjectList(labels);
+				titleButton.setVisibility(View.INVISIBLE);
 			} else {
 				List<AppLabel> apps = dbHelper.appsLabelDao.getApps(label.getId());
 				Collection<Application> newList = applicationInfoManager.convertToApplicationList(apps);
 				gridAdapter.setObjectList(new ArrayList<Application>(newList));
+
+				titleButton.setVisibility(View.VISIBLE);
+				titleButton.setOnClickListener(new OnClickListener() {
+					public void onClick(View v) {
+						chooseAppsDialogCreator.setCurrentLabel(label);
+						showDialog(chooseAppsDialogCreator.getDialogId());
+					}
+				});
 			}
 		}
 	}
@@ -145,5 +183,43 @@ public class LabelShortcut extends Activity implements DbChangeListener {
 
 	public void dataSetChanged() {
 		reloadGrid();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Hold on to this
+		// mMenu = menu;
+
+		// Inflate the currently selected menu XML resource.
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.home_menu, menu);
+		menu.getItem(0).setIcon(android.R.drawable.ic_menu_rotate);
+		// TODO info dialog
+		menu.getItem(1).setVisible(false);
+		menu.getItem(1).setIcon(android.R.drawable.ic_menu_info_details);
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.reload_apps:
+			new AppsReloader(this, false).reload();
+			return true;
+		case R.id.about:
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		genericDialogManager.onPrepareDialog(id, dialog);
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		return genericDialogManager.onCreateDialog(id);
 	}
 }
